850c21465bc524b18cfe58c9a9a5e0c35f94f024
Luis Angel Baltazar Gonzalez <luisangelbaltazarglz@outlook.com>
2026-04-18 15:10:56 -0600

    "base de datos"

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,10 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host:     process.env.DB_HOST,
+  user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port:     process.env.DB_PORT || 3306,
+  ssl:      { rejectUnauthorized: false }
 });
 
 export default pool;
